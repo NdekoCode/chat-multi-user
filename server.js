4b0c1d869bef886bf8406a081b4126fef1d441ca
Ndekocode <arickbulakali@gmail.com>
2022-11-20 17:00:43 +0200

    add socket initiation

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+import { Server } from "socket.io";
 import app from "./app.js"; // On importe App
 import { createServer } from "http"; // On importe la fonction de creation du serveur de node.js
 const PORT = process.env.PORT || 3500;
 const nodeServer = createServer(app);
+
+// IO
+const io = new Server(nodeServer);
+
+// TODO
+io.on("connection", (socket) => {});
 nodeServer.listen(PORT, () => {
   console.log("Port is listening at http://localhost:" + PORT);
 });
